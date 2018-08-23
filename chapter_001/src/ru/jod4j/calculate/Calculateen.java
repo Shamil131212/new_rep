3482e58aaf2aa69f8229533c2d0e413a288438c7
Shamil1312 <alievs1991@gmail.com>
2018-08-23 21:01:39 +0300

    1.4. JavDoc. Code convetion[#66352]

--- chapter_001/src/ru/jod4j/calculate/Calculateen.java
+++ chapter_001/src/ru/jod4j/calculate/Calculateen.java
@@ -1,6 +1,10 @@
 package ru.job4j.calculate;
 public class Calculate {
+	/** Это класс Main и тут я буду писать код
+	*/
 public static void main (String[]args){
+	/*Отсюда программа начинается
+	*/
 System.out.println("Hello World");
 }
 }
